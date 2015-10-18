08b748308dc6b9e914151e8aeed3521f9af3d4e5
TabBird <lrzlr2014@gmail.com>
2015-10-18 20:19:41 +0800

    add android platform

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,101 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
+
+// .config(function($compileProvider){
+//   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+// })
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+  // setup an abstract state for the tabs directive
+    .state('coach', {
+    url: '/coach',
+    abstract: true,
+    templateUrl: 'templates/coach.html'
+  })
+
+  // Each tab has its own nav history stack:
+    .state('coach.upload',{
+      url:'/upload',
+      views:{
+        'coach-upload':{
+          templateUrl:'templates/coach-idupload.html',
+          controller:'CoachIdUploadCtrl'          
+        }
+      }
+
+    })
+
+  .state('coach.home', {
+    url: '/home',
+    views: {
+      'coach-home': {
+        templateUrl: 'templates/coach-home.html',
+        controller: 'CoachHomeCtrl'
+      }
+    }
+  })
+
+  .state('coach.personalinfo', {
+      url: '/personalinfo',
+      views: {
+        'coach-personalinfo': {
+          templateUrl: 'templates/coach-personalinfo.html',
+          controller: 'CoachPersonalInfoCtrl'
+        }
+      }
+    })
+    .state('coach.config', {
+      url: '/config',
+      views: {
+        'coach-config': {
+          templateUrl: 'templates/coach-config.html',
+          controller: 'CoachPersonalConfigCtrl'
+        }
+      }
+    })
+
+  .state('coach.schedule', {
+    url: '/schedule',
+    views: {
+      'coach-schedule': {
+        templateUrl: 'templates/coach-schedule.html',
+        controller: 'CoachPersonalScheduleCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/coach/upload');
+
+});
---
+++ platforms/android/assets/www/js/services.js
@@ -0,0 +1,111 @@
+angular.module('starter.services', ['ngCordova'])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+// .factory('Camera', ['$q', function($q) {
+ 
+//   return {
+//     getPicture: function(options) {
+//       var q = $q.defer();
+      
+//       navigator.camera.getPicture(function(result) {
+//         // Do any magic you need
+//         q.resolve(result);
+//       }, function(err) {
+//         q.reject(err);
+//       }, options);
+      
+//       return q.promise;
+//     }
+//   }
+// }])
+
+// .factory('Camera', ['$cordovaCamera', function($cordovaCamera) {
+//     var options = { 
+//             quality : 75, 
+//             destinationType : Camera.DestinationType.DATA_URL, 
+//             sourceType : Camera.PictureSourceType.CAMERA, 
+//             allowEdit : true,
+//             encodingType: Camera.EncodingType.JPEG,
+//             targetWidth: 300,
+//             targetHeight: 300,
+//             popoverOptions: CameraPopoverOptions,
+//             saveToPhotoAlbum: false
+//         };
+//   return {
+//     getPicture: function() {
+//       $cordovaCamera.getPicture(options).then(function(imageData) {
+//             imgURI = "data:image/jpeg;base64," + imageData;
+//         }, function(err) {
+//             // An error occured. Show a message to the user
+//             console.log("sth wrong");
+//             imgURI = undefined;
+//         });
+//     }
+//   }
+// }])
+
+
+.factory('Storage', ['$window', function ($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage.setItem(key, value);
+    },
+    get: function(key) {
+      return $window.localStorage.getItem(key);
+    },
+    rm: function(key) {
+      $window.localStorage.removeItem(key);
+    },
+    clear: function() {
+      $window.localStorage.clear();
+    }
+  };
+}]);
